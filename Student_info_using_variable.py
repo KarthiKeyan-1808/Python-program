7843c2dab9db4ddb912d49d72daeb32f684b583c
Kartik-2018 <128675344+Kartik-2018@users.noreply.github.com>
2023-03-25 11:22:31 +0530

    Add files via upload

---
+++ Student_info_using_variable.py
@@ -0,0 +1,11 @@
+print("\tSTUDENT INFORMATION")
+print("- - - - - - - - - - - - - - - - - - - - ")
+#defining variables
+name="Karthi"
+age="29"
+email="krtkn.gmail"
+address="12, A madurai"
+mobile="945528555"
+#get the values by calling variables
+print("\tNAME\t: ",name, "\n\tAGE\t: ",age, "\n\tEMAIL\t: ",email, "\n\tADDRESS\t: ",address, "\n\tMOBILE\t: ",mobile)
+print("- - - - - - - - - - - - - - - - - - - - ")
