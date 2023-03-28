555e0e5f6a759b48bd5bd4c7607086c7a013abfb
Kartik-2018 <128675344+Kartik-2018@users.noreply.github.com>
2023-03-28 21:07:25 +0530

    Update Leave_letter.py

--- Leave_letter.py
+++ Leave_letter.py
@@ -1,22 +1,23 @@
 print("\t\t\t\tLeave Letter")
-name1=input("Name\n")
-std=input("\n\nStandard\n")
-school1=input("\n\nSchool\n")
-name2=input("Name\n")
-dpt=input("\n\nDepartment\n")
-school2=input("\n\nSchool\n")
-place=input("\n\nPlace\n")
-date=input("\n\nDate\n")
-print("From:","\n\t",name1,"\n\t",std,"\n\t",school1,"\n")
+name1=input("Please enter your name\t")
+std=input("Please enter your calss\t")
+school1=input("Please enter your school name\t")
+name2=input("Please enter your HOD name\t")
+dpt=input("Please enter their department\t")
+school2=input("Please enter the school name\t")
+reason=input("Please enter the reason\t")
+gender=input("please enter Sir or Madem\t")
+place=input("Place\t")
+date=input("Date\t")
+print("\n\nFrom:","\n\t",name1,"\n\t",std,"\n\t",school1,"\n")
 print("To:","\n\t",name2,"\n\t",dpt,"\n\t",school2,"\n")
-print("Respected sir/mam,")
+print("Respected", gender,"\n")
 print('''\tI am writting this letter to seek your
 \tapproval for leave on 24-03-2023 due to
 \tan important personal reason. I hope you
 \tunderstand my situation, please approve
 \tmy leave request.
-
-\t\tThanking you.
+\t\t\nThanking you.
 ''')
-print("Place\n",place,"\nDate\n",date,"\t\t\t\tYours obediently\n\t\t\t\t",name1)
+print(place,"\n",date,"\t\t\t\tYours obediently\n\t\t\t\t\t",name1)
 
