41565643bd2f9bb2f0dbb55565701fb73e14ebca
Kartik-2018 <128675344+Kartik-2018@users.noreply.github.com>
2023-03-28 12:02:45 +0530

    Add files via upload

---
+++ Leave_letter.py
@@ -0,0 +1,22 @@
+print("\t\t\t\tLeave Letter")
+name1=input("Name\n")
+std=input("\n\nStandard\n")
+school1=input("\n\nSchool\n")
+name2=input("Name\n")
+dpt=input("\n\nDepartment\n")
+school2=input("\n\nSchool\n")
+place=input("\n\nPlace\n")
+date=input("\n\nDate\n")
+print("From:","\n\t",name1,"\n\t",std,"\n\t",school1,"\n")
+print("To:","\n\t",name2,"\n\t",dpt,"\n\t",school2,"\n")
+print("Respected sir/mam,")
+print('''\tI am writting this letter to seek your
+\tapproval for leave on 24-03-2023 due to
+\tan important personal reason. I hope you
+\tunderstand my situation, please approve
+\tmy leave request.
+
+\t\tThanking you.
+''')
+print("Place\n",place,"\nDate\n",date,"\t\t\t\tYours obediently\n\t\t\t\t",name1)
+
