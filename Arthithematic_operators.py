0be436acbf39021a9f7165808c87ec7067238743
Kartik-2018 <128675344+Kartik-2018@users.noreply.github.com>
2023-03-29 11:54:34 +0530

    Add files via upload

---
+++ Arthithematic_operators.py
@@ -0,0 +1,34 @@
+#Area of Rectangle
+print("Area of Rectangle")
+print("-----------------")
+l=int(input("Please enter the value of l "))
+b=int(input("Please enter the value of b "))
+rectangle=l*b
+print("\nArea of Rectangle =",rectangle)
+print("-----------------")
+#Area of square
+print("\n\nArea of Square")
+print("------------------")
+a=int(input("Please enter the value of a "))
+square=a*a
+print("\nArea of Square =",square)
+print("------------------")
+
+#Area of Triangle
+print("\n\nArea of Triangle")
+print("--------------------")
+b=int(input("Please enter the value of b "))
+h=int(input("Please enter the value of h "))
+triangle=1/2*b*h
+print("\nArea of Triangle =",triangle)
+print("--------------------")
+
+#Simple intrest
+print("\n\nCalculate Simple Intrest")
+print("----------------------------")
+p=int(input("Please enter the principle amount "))
+n=int(input("Please enter the number of year "))
+r=int(input("Please enter the rate of intrest "))
+simple_intrest=p*n*r/100
+print("\nSimple Intrest =",simple_intrest)
+print("----------------------------")
