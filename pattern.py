6904fa3c8f25ebcb6d75babcbba7bbd338017da4
Kartik-2018 <128675344+Kartik-2018@users.noreply.github.com>
2023-04-13 10:53:08 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,40 @@
+## 1 2 3
+## 1 2 3
+## 1 2 3
+
+##rows=int(input("Please enter the rows "))
+##columns=int(input("Please enter the columns "))
+##for i in range(1, rows+1):
+##    for j in range(1, columns+1):
+##        print(j,end=" ")
+##    print()
+
+## 1 1 1
+## 2 2 2
+## 3 3 3
+
+##rows=int(input("Please enter the rows "))
+##columns=int(input("Please enter the columns "))
+##for i in range(1, rows+1):
+##    for j in range(1, columns+1):
+##        print(i,end=" ")
+##    print()
+
+## 1 2 3
+## 4 5 6
+## 7 8 9
+
+rows=int(input("Please enter the rows "))
+columns=int(input("Please enter the columns "))
+for i in range(1, rows+1):
+    for j in range(1, columns+1):
+        print(i+j,end=" ")
+    print()
+
+
+
+
+
+
+
+
