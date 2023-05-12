6abe75e057c965e3d52ad56b5124a17611caf771
Kartik-2018 <128675344+Kartik-2018@users.noreply.github.com>
2023-05-12 12:03:09 +0530

    Add files via upload

---
+++ dict_problems.py
@@ -0,0 +1,20 @@
+#Student mark
+##student={}
+##length=int(input("Please enter the length"))
+##for i in range (length):
+##    name=input("Please enter the name")
+##    mark=int(input("Please enter the mark"))
+##    if mark>35:
+##        student[name]=mark
+##print(student)
+
+#two list to dictionary
+##dict1={}
+##l1=[1,3,7]
+##l2=[10,2,8]
+##for i in range(len(l1)):
+##    keys=l1[i]
+##    dict1[keys]=l2[i]
+##print(dict1)
+
+
