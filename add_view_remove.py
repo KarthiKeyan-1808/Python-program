304ba665f3fbc95bc3d1e10711b02409f92a3613
Kartik-2018 <128675344+Kartik-2018@users.noreply.github.com>
2023-06-16 08:18:52 +0530

    add_view_remove.py

---
+++ add_view_remove.py
@@ -0,0 +1,40 @@
+names = []
+while True:
+    print("Enter 1 to add name")
+    print("Enter 2 to view names")
+    print("Enter 3 to remove name")
+    print("Enter 4 to exit")
+
+    choice=int(input("Please enter your choice "))
+
+    def add():
+        count=int(input("How many names you would like to enter "))
+        for i in range(count):
+            name=input("Please enter the name: ")
+            names.append(name)
+
+    def view():
+        print("Names:")
+        for i in names:
+            print(i)
+
+    def remove():
+        name=input("Please enter the name to remove: ")
+        if name in names:
+            names.remove(name)
+        else:
+            print("Name not found")
+
+    def exit():
+        print("Thanks!")
+
+
+    if choice==1:
+        add()
+    if choice==2:
+        view()
+    if choice==3:
+        remove()
+    if choice==4:
+        exit()
+        break
